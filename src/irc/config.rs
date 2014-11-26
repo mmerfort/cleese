4ba796d507577c03cff144b215d53a6f1ad5e0dc
Andrew Brinker <me@andrewbrinker.com>
2014-11-25 16:23:55 -0800

    Added more documentation

--- src/irc/config.rs
+++ src/irc/config.rs
@@ -1,11 +1,16 @@
 // For copyright information, see the LICENSE.md folder at the top of this
 // project's directory structure.
 
+//! # Config
+//!
+//! Configuration is loaded from a json file. The only difference between
+//! JsonConfig and IrcConfig is that the IrcConfig doesn't own its strings.
+
 use regex::Regex;
 use serialize::{json, Decodable};
 use std::io::{File, Open, Read};
 
-// Single server for now.
+/// This configuration matches up exactly with what you see in the config file.
 pub struct IrcConfig<'a> {
     pub host: &'a str,
     pub port: u16,
@@ -17,6 +22,8 @@ pub struct IrcConfig<'a> {
     pub cmd_prefix: &'a str,
 }
 
+/// Used only for decoding the .json file. The configuration ends up being used
+/// as an IrcConfig.
 #[deriving(Decodable)]
 pub struct JsonConfig {
     pub host: String,
@@ -30,6 +37,7 @@ pub struct JsonConfig {
 }
 
 impl JsonConfig {
+    /// Attempt to open and decode the given configuration file.
     pub fn new(location: String) -> JsonConfig {
         let p = Path::new(location.as_slice());
         let mut file = match File::open_mode(&p, Open, Read) {
