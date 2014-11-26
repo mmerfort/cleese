4c899e47151742282576d3c90be4445fec891f06
Andrew Brinker <me@andrewbrinker.com>
2014-11-25 16:30:59 -0800

    Added Command documentation

--- src/irc/command.rs
+++ src/irc/command.rs
@@ -1,10 +1,21 @@
 // For copyright information, see the LICENSE.md folder at the top of this
 // project's directory structure.
 
+//! # Command
+//!
+//! This defines the structure and processing of commands. Every command is
+//! constructed in the following way:
+//!
+//!     prefix command args...
+//!
+//! The prefix may be any length, and the command is only one word. The prefix
+//! is what identifies the text as a command. The rest (command and args) are
+//! passed through to the plugin subsystem and processed there.
+
 use irc::privmsg::*;
 use util::*;
 
-// Command through irc.
+/// Command through IRC.
 #[deriving(Show)]
 pub struct IrcCommand<'a> {
     pub name: &'a str,
@@ -13,6 +24,7 @@ pub struct IrcCommand<'a> {
 }
 
 impl<'a> IrcCommand<'a> {
+    /// Construct an IRC command as a simple command with an associated channel.
     pub fn new(msg: &'a IrcPrivMsg, key: &'a str) -> Option<IrcCommand<'a>> {
         match Command::new(msg.txt.as_slice(), key) {
             Some(cmd) => {
@@ -27,6 +39,7 @@ impl<'a> IrcCommand<'a> {
     }
 }
 
+/// A command without a channel. Otherwise analogous to IrcCommand.
 #[deriving(Show)]
 pub struct Command<'a> {
     pub name: &'a str,
@@ -34,6 +47,7 @@ pub struct Command<'a> {
 }
 
 impl<'a> Command<'a> {
+    /// Attempts to process the command into name and arguments. This may fail.
     pub fn new(s: &'a str, key: &'a str) -> Option<Command<'a>> {
         let s = s.trim();
         if s.len() > 0 && s.starts_with(key) {
